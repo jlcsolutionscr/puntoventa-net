81e28d39d82148b5a866e6e5e85950d5e341797d
Jason Lopez <Jason.Lopez@go-afs.com>
2021-11-04 12:39:13 +0000

    Release to production

--- ServicioWeb/Endpoints/Properties/AssemblyInfo.cs
+++ ServicioWeb/Endpoints/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados (número de compilación y de revisión) 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.4.0.0")]
-[assembly: AssemblyFileVersion("4.4.0.0")]
+[assembly: AssemblyVersion("4.4.2.0")]
+[assembly: AssemblyFileVersion("4.4.2.0")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
