16c646186c7eccad1a9261a90c9bdac5c36dc9dd
Jason Lopez <Jason.Lopez@go-afs.com>
2022-05-18 22:04:17 +0000

    Adeed company id to invoice generation error log entries

--- WebAPI/Servicios/FacturacionService.cs
+++ WebAPI/Servicios/FacturacionService.cs
@@ -337,6 +337,7 @@ namespace LeandroSoftware.ServicioWeb.Servicios
                 decimal decTipoDeCambio = 1;
                 try
                 {
+                    if (factura.IdVendedor == 0) throw new BusinessException("La factura no posee el identificador del vendedor. Por favor verifique la información.");
                     Empresa empresa = dbContext.EmpresaRepository.Include("PlanFacturacion").Where(x => x.IdEmpresa == factura.IdEmpresa).FirstOrDefault();
                     if (empresa == null) throw new BusinessException("Empresa no registrada en el sistema. Por favor, pongase en contacto con su proveedor del servicio.");
                     if (!empresa.PermiteFacturar) throw new BusinessException("La empresa que envía la transacción no se encuentra activa en el sistema de facturación electrónica. Por favor, pongase en contacto con su proveedor del servicio.");
@@ -394,12 +395,6 @@ namespace LeandroSoftware.ServicioWeb.Servicios
                         }
                     }
                     factura.TipoDeCambioDolar = decTipoDeCambio;
-                    if (terminal.IdTipoDispositivo == StaticTipoDispisitivo.AppMovil)
-                    {
-                        Vendedor vendedor = dbContext.VendedorRepository.Where(x => x.IdEmpresa == factura.IdEmpresa).FirstOrDefault();
-                        if (vendedor == null) throw new BusinessException("La empresa no posee registrado ningún vendedor");
-                        factura.IdVendedor = vendedor.IdVendedor;
-                    }
                     Cliente cliente = dbContext.ClienteRepository.Find(factura.IdCliente);
                     if (cliente == null) throw new BusinessException("El cliente asignado a la factura no existe.");
                     if (cliente.IdCliente > 1)
@@ -757,7 +752,7 @@ namespace LeandroSoftware.ServicioWeb.Servicios
                 catch (Exception ex)
                 {
                     dbContext.RollBack();
-                    _logger.LogError("Error al agregar el registro de facturación: ", ex);
+                    _logger.LogError("Error al agregar el registro de facturación para la empresa: " + factura.IdEmpresa, ex);
                     throw new Exception("Se produjo un error guardando la información de la factura. Por favor consulte con su proveedor.");
                 }
             }
